04b85a72e20adbec22fb2025ca14cc93dfef598e
kaldren <drenski666@hotmail.com>
2019-01-18 07:32:00 +0200

    CRUD Interface repository

---
+++ Meaoc-API/Domain/Repository/IRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meaoc_API.Domain.Repository
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// Get entity by identifier
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TEntity GetById(object id);
+
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(TEntity entity);
+
+        /// <summary>
+        /// Insert entities
+        /// </summary>
+        /// <param name="entities"></param>
+        void Insert(IEnumerable<TEntity> entities);
+
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name=""></param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Update entities
+        /// </summary>
+        /// <param name="entities"></param>
+        void Update(IEnumerable<TEntity> entities);
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// Delete entities
+        /// </summary>
+        /// <param name="entities"></param>
+        void Delete(IEnumerable<TEntity> entities);
+    }
+}
